08dc97e4ed64d1890153a202366261567998f17e
jocideak <114059988+jocideak@users.noreply.github.com>
2022-11-24 09:33:57 +0100

    blog id

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.blog.id
+          target_id = aws_instance.blog_id
           port = 80
         }
       }
